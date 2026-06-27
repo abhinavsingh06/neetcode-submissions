7d78ab32874cdad6bc99375067447377b11be372
Abhinav Singh <singh.abhinav6996@gmail.com>
2026-06-28 02:22:02 +0530

    Update: two-integer-sum-ii - submission-0

--- Data Structures & Algorithms/two-integer-sum-ii/submission-0.js
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.js
@@ -6,18 +6,18 @@ class Solution {
      */
     twoSum(numbers, target) {
         let left = 0;
-        let right = numbers.length-1;
+        let right = numbers.length -1;
         while(left<right){
             let sum = numbers[left] + numbers[right];
 
-            if(sum === target){
+            if(sum == target){
                 return [left + 1, right + 1];
-            } else if(sum < target){
-                left++;
+            }
+            else if(sum<target){
+                left ++;
             } else{
-                right--
+                right --
             }
         }
-        
     }
 }
